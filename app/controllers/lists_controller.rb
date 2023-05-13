46f96a9319b5db4a4aecaf53f85bcc35dae33bd6
EC2 Default User <ec2-user@ip-172-31-22-93.ap-northeast-1.compute.internal>
2023-05-13 14:21:47 +0000

    フラッシュメッセージ実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,8 @@
 class ListsController < ApplicationController
   def new
-    @list = List.new 
+    @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
@@ -20,28 +20,29 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to '/lists'
   end
-  
+
   def create
      @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "投稿が成功しました"
       redirect_to list_path(@list.id)
     else
       render :new
     end
   end
-  
+
   private
 
   def list_params
